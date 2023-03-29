ef545e108f633f7d95977c9fd5b3abc4778f2972
MoranMishan <mishanmo@gmail.com>
2023-03-29 14:51:08 +0300

    Delete client directory

--- client/src/utils/data-utils.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-export const getWordCloudData = async <T>(url: string): Promise<T> => {
-	const res = await axios.get(url);
-	console.log(res.data?.result);
-	return res.data?.result;
-}
