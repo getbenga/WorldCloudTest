7ce8a4122368dccb52a4314874d600a4f9c3d550
MoranMishan <mishanmo@gmail.com>
2023-03-29 14:51:21 +0300

    Delete api directory

--- api/routes.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import {getWordCloud} from "./wordCloudController";
-
-const router = express.Router({ mergeParams: true });
-
-router.route('/word_cloud').get(getWordCloud);
-
-export default router;
--- api/utils.ts
+++
@@ -1,8 +0,0 @@
-export const formattedWordCloud = (results: {[key: string]: number}) => {
-	const formattedResults: {text: string, value: number}[]= [];
-	Object.keys(results).forEach((key) => {
-		formattedResults.push({text: key, value: results[key]})
-	})
-
-	return formattedResults;
-}
